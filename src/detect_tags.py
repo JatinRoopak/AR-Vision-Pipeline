79809d98fc1ccffe05f0db56925bd58a9423fd6a
JatinRoopak <jatin.roopak.xb.8195@gmail.com>
2026-02-12 19:58:11 +0530

    we can detect multiplt tags in one frame now and increase the robustness of detecting tags using more filters.

--- src/detect_tags.py
+++ src/detect_tags.py
@@ -13,23 +13,44 @@ def order_points(pts): #homography require matched pairs of corner points to map
     return rectangle
 
 def get_tag_corners(bianry_img):
-    contours, _ = cv2.findContours(bianry_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, herarchy = cv2.findContours(bianry_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     candidates = []
 
-    for cnt in contours:
+    if herarchy is None:
+        return []
+
+    herarchy = herarchy[0] 
+    candidates = []
+
+    for i, cnt in enumerate(contours):
+        area = cv2.contourArea(cnt)
+
+        if area < 1000: #ignore tiny noises
+            continue
         if cv2.contourArea(cnt) < 1000:
             continue
+        
+        current_herarchy = herarchy[i]
+        if current_herarchy[2] == -1: #if shape has no data inside it ignore it 
+            continue
 
+        #shape approximation filter
         epsilon = 0.02*cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, epsilon, True)
 
         if len(approx) == 4:
             pts = approx.reshape(4, 2)
-
             ordered_pts = order_points(pts)
 
-            area = cv2.contourArea(cnt)
-            candidates.append((area, ordered_pts))
+            width_A = np.linalg.norm(ordered_pts[0]-ordered_pts[1])
+            height_A = np.linalg.norm(ordered_pts[0]-ordered_pts[3])
+
+            if height_A == 0:
+                continue
+            aspect_ratio = width_A/height_A
+
+            if 0.8 <= aspect_ratio <= 1.2: #check aspect ratio
+                candidates.append((area, ordered_pts))
 
     candidates.sort(key=lambda x: x[0], reverse=True) #only take the tag which is the largest
     return [c[1] for c in candidates]
--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from decode_tags import split_into_smaller_grid, find_tag_id
 
 
 def main():
-    camera = cv2.VideoCapture(0) #open webcam
+    camera = cv2.VideoCapture(2) #open webcam
 
     if not camera.isOpened():
         print('Camera cannot be opened')
@@ -31,9 +31,8 @@ def main():
         active_tag_found = False
 
         if len(candidates) > 0:
-            for i in range(min(len(candidates), 3)): #run for largest 3 tags found
-
-                tag_corners = candidates[0]
+            for i, tag_corners in enumerate(candidates[:10]): #run for largest 10 potrntial tags found
+                tag_corners = candidates[i]
                 flat_corner = np.array([
                     [0,0],
                     [160, 0],
@@ -64,20 +63,10 @@ def main():
                         cv2.imshow("4, unwarped tag", flat_tag)
                         pts = tag_corners.astype(int)
                         cv2.polylines(debug_frame, [pts.reshape(-1,1,2)], True, (0,0,255), 5) #paint main tag boxes red
-                        break
+                        
                     else:
                         print(tag_grid)
                         print("decoding is failing")
-      
-                
-        for points in candidates:
-            points = points.astype(int)
-            cv2.polylines(debug_frame, [points.reshape(-1, 1, 2)], True, (0, 255, 0), 3) #draw green lines around box
-            
-            labels = ["0", "1", "2", "3"]
-            for i, (x,y) in enumerate(points):
-                cv2.circle(debug_frame, (x, y), 5, (0, 0, 255), -1)
-                cv2.putText(debug_frame, labels[i], (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
         cv2.imshow("AR Tag Detection", debug_frame)
         cv2.imshow("Edge Map", edges)
