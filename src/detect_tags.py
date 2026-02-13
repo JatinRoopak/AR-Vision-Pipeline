23c088876040439cb7d703a7f9345482eff8c939
JatinRoopak <jatin.roopak.xb.8195@gmail.com>
2026-02-13 05:32:18 +0530

    FIX BUG: the tags do not get detected multiple times and also tags with same ID in single frame can be detected without fail. Change code structure to make it more organised

--- src/detect_tags.py
+++ src/detect_tags.py
@@ -1,6 +1,11 @@
 import cv2 
 import numpy as np
 
+from transformation import homography, warp_perspective_fast
+from sobel_edge import sobel_edge_detect
+from filter_tags import greyscale, gaussian_smoothing
+from decode_tags import split_into_smaller_grid, find_tag_id
+
 def order_points(pts): #homography require matched pairs of corner points to map correctly
     rectangle = np.zeros((4, 2), dtype="float32")
     s = pts.sum(axis = 1)
@@ -12,8 +17,8 @@ def order_points(pts): #homography require matched pairs of corner points to map
 
     return rectangle
 
-def get_tag_corners(bianry_img):
-    contours, herarchy = cv2.findContours(bianry_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+def get_tag_corners(binary_img):
+    contours, herarchy = cv2.findContours(binary_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     candidates = []
 
     if herarchy is None:
@@ -54,3 +59,46 @@ def get_tag_corners(bianry_img):
 
     candidates.sort(key=lambda x: x[0], reverse=True) #only take the tag which is the largest
     return [c[1] for c in candidates]
+
+def detect_ar_tags(frame):
+    detected_tags = []
+
+    greyscaled_frame = greyscale(frame)
+    gaussian_smoothed_frame = gaussian_smoothing(greyscaled_frame)
+    edges = sobel_edge_detect(gaussian_smoothed_frame)
+    candidates = get_tag_corners(edges)
+
+    if len(candidates) > 0:
+        for tag_corners in candidates[:10]: #run for largest 10 potrntial tags found
+            # tag_corners = candidates[i]
+            flat_corner = np.array([
+                [0,0],
+                [160, 0],
+                [160, 160],
+                [0, 160]
+            ], dtype="float32")
+
+            H = homography(tag_corners, flat_corner)
+
+            if H is not None:
+                #warping the color frame to see tag upright
+                flat_tag = warp_perspective_fast(frame, H, (160, 160))
+
+                #finding the id
+                tag_grid = split_into_smaller_grid(flat_tag)
+                tag_id, n_rotations = find_tag_id(tag_grid)
+
+                if tag_id is not None:
+                    fixed_corners = np.roll(tag_corners, -n_rotations, axis=0) #for the top left corner to always be at indec 0 rotate
+                    
+                    tag_info = {
+                        "id": tag_id,
+                        "corners": fixed_corners,   # The 4 corners in correct order
+                        "center": (int(np.mean(tag_corners[:, 0])), int(np.mean(tag_corners[:, 1]))),
+                        "homography": H,
+                        "warped_image": flat_tag,
+                        "orientation": n_rotations
+                    }
+                    detected_tags.append(tag_info)
+    
+    return detected_tags, edges
--- src/main.py
+++ src/main.py
@@ -1,15 +1,11 @@
 import cv2
 import numpy as np
 
-from filter_tags import greyscale, gaussian_smoothing
-from sobel_edge import sobel_edge_detect
-from detect_tags import get_tag_corners
-from transformation import homography, warp_perspective_fast
-from decode_tags import split_into_smaller_grid, find_tag_id
-
+from detect_tags import detect_ar_tags
 
 def main():
-    camera = cv2.VideoCapture(2) #open webcam
+    camera = cv2.VideoCapture(0) #open webcam
+    # camera = cv2.VideoCapture('multipleTags.mp4') #use pre shooted video
 
     if not camera.isOpened():
         print('Camera cannot be opened')
@@ -20,56 +16,49 @@ def main():
         if not ret:
             break
 
-        grayscaled_frame = greyscale(frame) #do greyscaling
-        smoothed_frame = gaussian_smoothing(grayscaled_frame) #smoothout for better edge detection
-        edges = sobel_edge_detect(smoothed_frame) #applying edge detection
-
-        candidates = get_tag_corners(edges)
-
+        tags, edge = detect_ar_tags(frame)
         debug_frame = frame.copy()
 
-        active_tag_found = False
+        seen_centers = []
+        current_warped_tag = None
 
-        if len(candidates) > 0:
-            for i, tag_corners in enumerate(candidates[:10]): #run for largest 10 potrntial tags found
-                tag_corners = candidates[i]
-                flat_corner = np.array([
-                    [0,0],
-                    [160, 0],
-                    [160, 160],
-                    [0, 160]
-                ], dtype="float32")
+        for tag in tags:
+            tag_id = tag["id"]
+            corners = tag["corners"].astype(int)
+            cx, cy = tag["center"]
 
-                H = homography(tag_corners, flat_corner)
+            #checking mechnism for the tags if 2 tags have same id they still be detected deferently
+            is_duplicate = False
+            for(existing_cx, existing_cy) in seen_centers:
+                distance = np.sqrt((cx - existing_cx)**2 + (cy - existing_cy)**2)
+                if distance<20: 
+                    is_duplicate = True
+                    break
+            if is_duplicate:
+                continue
+            seen_centers.append((cx, cy))
 
-                if H is not None:
-                    #warping the color frame to see tag upright
-                    flat_tag = warp_perspective_fast(frame, H, (160, 160))
+            if "warped_image" in tag:
+                current_warped_tag = tag["warped_image"]
 
-                    #finding the id
-                    tag_grid = split_into_smaller_grid(flat_tag)
-                    tag_id, n_rotations = find_tag_id(tag_grid)
+            #Info Graphics
+            cv2.polylines(debug_frame, [corners.reshape(-1,1,2)], True, (0, 0, 255), 3)
 
-                    if tag_id is not None:
-                        fixed_corners = np.roll(tag_corners, -n_rotations, axis=0) #for the top left corner to always be at indec 0 rotate
-                        
-                        #info_grafics
-                        cx, cy = int(np.mean(tag_corners[:, 0])), int(np.mean(tag_corners[:, 1]))
-                        cv2.putText(debug_frame, f"ID: {tag_id}", (cx-20, cy), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-                        first_pt = fixed_corners[0].astype(int)
-                        cv2.circle(debug_frame, tuple(first_pt), 10, (255, 0, 0), -1)
+            cv2.putText(debug_frame, f"ID: {tag_id}", (cx-20, cy), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
-                        cv2.imshow("4, unwarped tag", flat_tag)
-                        pts = tag_corners.astype(int)
-                        cv2.polylines(debug_frame, [pts.reshape(-1,1,2)], True, (0,0,255), 5) #paint main tag boxes red
-                        
-                    else:
-                        print(tag_grid)
-                        print("decoding is failing")
+            cv2.circle(debug_frame, tuple(corners[0]), 10, (255, 0, 0), -1)
 
+            labels = ["0", "1", "2", "3"]
+            for i, point in enumerate(corners):
+                cv2.putText(debug_frame, labels[i], (point[0] - 10, point[1] - 10), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+        
+        cv2.imshow("Edge Map", edge)
         cv2.imshow("AR Tag Detection", debug_frame)
-        cv2.imshow("Edge Map", edges)
+
+        if current_warped_tag is not None:
+            cv2.imshow("Warped Tag", current_warped_tag)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
